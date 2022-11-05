d0b8427283ba5d32e8b108c683c887048b597834
Zhiyuan Ma <631253076@qq.com>
2022-11-05 07:31:28 -0400

    codeforces 1516C

---
+++ 1516/C. Baby Ehab Partitions Again/Solution.java
@@ -0,0 +1,151 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        Kattio kattio = new Kattio();
+//        int t = kattio.nextInt();
+//        while (t > 0) {
+//            solve(kattio);
+//            t--;
+//        }
+        solve(kattio);
+        kattio.close();
+    }
+
+    static int[] dirs = new int[]{0, 1, 0, -1, 0};
+    static List<Integer> primes;
+    static int[] count;
+
+    public static void solve(Kattio kattio) {
+        int n = kattio.nextInt();
+        int[] a = new int[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            a[i] = kattio.nextInt();
+            sum += a[i];
+        }
+        if (sum % 2 == 1) {
+            kattio.println(0);
+        } else {
+            boolean[] can = new boolean[sum / 2 + 1];
+            can[0] = true;
+            for (int num : a) {
+                for (int i = sum / 2; i >= 0; i--) {
+                    if (can[i] && i + num <= sum / 2) {
+                        can[i + num] = true;
+                    }
+                }
+            }
+            if (can[sum / 2]) {
+                kattio.println(1);
+                for (int i = 0; i < n; i++) {
+                    if (a[i] % 2 == 1 || !can[(sum - a[i]) / 2]) {
+                        kattio.println(i + 1);
+                        return;
+                    }
+                }
+            } else {
+                kattio.println(0);
+            }
+        }
+    }
+
+    public static List<Integer> getPrime(int max) {
+        boolean[] notP = new boolean[max + 1];
+        List<Integer> res = new ArrayList<>();
+        for (int i = 2; i <= max; i++) {
+            if (notP[i]) {
+                continue;
+            }
+            res.add(i);
+            for (int j = 1; j * i <= max; j++) {
+                notP[j * i] = true;
+            }
+        }
+        return res;
+    }
+
+    public static int countFactors(int num) {
+        int res = 0;
+        for (Integer prime : primes) {
+            if (prime * prime > num) {
+                break;
+            }
+            while (num % prime == 0) {
+                num /= prime;
+                res++;
+            }
+        }
+        if (num > 1) {
+            res++;
+        }
+        return res;
+    }
+
+    public static int gcd(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static String toBinary(int num) {
+        return String.format("%16s", Integer.toBinaryString(num)).replace(" ", "0");
+    }
+
+    static class Kattio extends PrintWriter {
+        BufferedReader r;
+        StringTokenizer st;
+
+        // 标准 IO
+        Kattio() {
+            this(System.in, System.out);
+        }
+
+        Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+
+        // 文件 IO
+        Kattio(String input, String output) throws IOException {
+            super(output);
+            r = new BufferedReader(new FileReader(input));
+        }
+
+        // 在没有其他输入时返回 null
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    st = new StringTokenizer(r.readLine());
+                }
+                return st.nextToken();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
