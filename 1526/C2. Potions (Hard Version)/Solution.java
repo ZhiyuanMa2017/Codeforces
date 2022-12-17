a4a76ee8d4997fabde5c5d5eb1ffc696e1a8343c
Zhiyuan Ma <631253076@qq.com>
2022-12-16 23:47:47 -0500

    codeforces 1526C2

---
+++ 1526/C2. Potions (Hard Version)/Solution.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        Kattio kattio = new Kattio();
+//        int t = kattio.nextInt();
+//        while (t > 0) {
+//            solve(kattio);
+//            t--;
+//        }
+        solve(kattio);
+        kattio.close();
+    }
+
+    static int mod = 1000000007;
+
+    public static void solve(Kattio kattio) {
+        int n = kattio.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        long cur = 0;
+        for (int i = 0; i < n; i++) {
+            int num = kattio.nextInt();
+            cur += num;
+            pq.offer(num);
+            if (cur < 0) {
+                cur -= pq.poll();
+            }
+        }
+        kattio.println(pq.size());
+    }
+
+    static class Kattio extends PrintWriter {
+        BufferedReader r;
+        StringTokenizer st;
+
+        // 标准 IO
+        Kattio() {
+            this(System.in, System.out);
+        }
+
+        Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+
+        // 文件 IO
+        Kattio(String input, String output) throws IOException {
+            super(output);
+            r = new BufferedReader(new FileReader(input));
+        }
+
+        // 在没有其他输入时返回 null
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    st = new StringTokenizer(r.readLine());
+                }
+                return st.nextToken();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
