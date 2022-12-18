2317ab3c7f8e1109953f36262605c4cce5cbd555
Zhiyuan Ma <631253076@qq.com>
2022-12-17 23:46:30 -0500

    codeforces 1358C

---
+++ 1358/C. Celex Update/Solution.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) {
+        Kattio kattio = new Kattio();
+        int t = kattio.nextInt();
+        while (t > 0) {
+            solve(kattio);
+            t--;
+        }
+//        solve(kattio);
+        kattio.close();
+    }
+
+    static int mod = 1000000007;
+
+    public static void solve(Kattio kattio) {
+        long x1 = kattio.nextLong();
+        long y1 = kattio.nextLong();
+        long x2 = kattio.nextLong();
+        long y2 = kattio.nextLong();
+        kattio.println((x2 - x1) * (y2 - y1) + 1);
+    }
+
+    static class Kattio extends PrintWriter {
+        BufferedReader r;
+        StringTokenizer st;
+
+        // 标准 IO
+        Kattio() {
+            this(System.in, System.out);
+        }
+
+        Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+
+        // 文件 IO
+        Kattio(String input, String output) throws IOException {
+            super(output);
+            r = new BufferedReader(new FileReader(input));
+        }
+
+        // 在没有其他输入时返回 null
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    st = new StringTokenizer(r.readLine());
+                }
+                return st.nextToken();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
