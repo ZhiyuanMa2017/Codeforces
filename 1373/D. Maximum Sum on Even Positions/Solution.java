f5772df342d542edbc3f7f1d4c98697ec72910c2
Zhiyuan Ma <631253076@qq.com>
2022-12-25 05:39:51 -0500

    codeforces 1373D

---
+++ 1373/D. Maximum Sum on Even Positions/Solution.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) {
+        Kattio kattio = new Kattio();
+        int t = kattio.nextInt();
+        while (t > 0) {
+            solve(kattio);
+            t--;
+        }
+//        solve(kattio);
+        kattio.close();
+    }
+
+    public static void solve(Kattio kattio) {
+        int n = kattio.nextInt();
+        int[] a = new int[n];
+        long even = 0;
+        for (int i = 0; i < n; i++) {
+            a[i] = kattio.nextInt();
+            if (i % 2 == 0) {
+                even += a[i];
+            }
+        }
+        long diff = 0;
+        long cur = 0;
+        for (int i = 1; i < n; i += 2) {
+            cur += a[i] - a[i - 1];
+            diff = Math.max(diff, cur);
+            if (cur < 0) {
+                cur = 0;
+            }
+        }
+        cur = 0;
+        for (int i = 1; i + 1 < n; i += 2) {
+            cur += a[i] - a[i + 1];
+            diff = Math.max(diff, cur);
+            if (cur < 0) {
+                cur = 0;
+            }
+        }
+        kattio.println(even + diff);
+    }
+
+
+    static class Kattio extends PrintWriter {
+        BufferedReader r;
+        StringTokenizer st;
+
+        // 标准 IO
+        Kattio() {
+            this(System.in, System.out);
+        }
+
+        Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+
+        // 文件 IO
+        Kattio(String input, String output) throws IOException {
+            super(output);
+            r = new BufferedReader(new FileReader(input));
+        }
+
+        // 在没有其他输入时返回 null
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    st = new StringTokenizer(r.readLine());
+                }
+                return st.nextToken();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
