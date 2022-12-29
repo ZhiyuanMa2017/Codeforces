855cf97aabf23790fafd8e65304831c580e23e8d
Zhiyuan Ma <631253076@qq.com>
2022-12-28 23:43:59 -0500

    codeforces 1519D

---
+++ 1519/D. Maximum Sum of Products/Solution.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) {
+        Kattio kattio = new Kattio();
+//        int t = kattio.nextInt();
+//        while (t > 0) {
+//            solve(kattio);
+//            t--;
+//        }
+        solve(kattio);
+        kattio.close();
+    }
+
+    public static void solve(Kattio kattio) {
+        int n = kattio.nextInt();
+        long[] a = new long[n + 1];
+        long[] b = new long[n + 1];
+        long sum = 0;
+        for (int i = 1; i <= n; i++) {
+            a[i] = kattio.nextInt();
+        }
+        for (int i = 1; i <= n; i++) {
+            b[i] = kattio.nextInt();
+            sum += a[i] * b[i];
+        }
+        long[][] dp = new long[n + 1][n + 1];
+        long max = 0;
+        for (int len = 2; len <= n; len++) {
+            for (int l = 1; l + len - 1 <= n; l++) {
+                int r = l + len - 1;
+                dp[l][r] = dp[l + 1][r - 1] - a[l] * b[l] - a[r] * b[r] + a[l] * b[r] + a[r] * b[l];
+                max = Math.max(max, dp[l][r]);
+            }
+        }
+        kattio.println(sum + max);
+    }
+
+
+    static class Kattio extends PrintWriter {
+        BufferedReader r;
+        StringTokenizer st;
+
+        // 标准 IO
+        Kattio() {
+            this(System.in, System.out);
+        }
+
+        Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+
+        // 文件 IO
+        Kattio(String input, String output) throws IOException {
+            super(output);
+            r = new BufferedReader(new FileReader(input));
+        }
+
+        // 在没有其他输入时返回 null
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    st = new StringTokenizer(r.readLine());
+                }
+                return st.nextToken();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
