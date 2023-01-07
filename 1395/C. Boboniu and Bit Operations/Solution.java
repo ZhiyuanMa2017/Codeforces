65fedc3d4c1a1d3fdab474b9fbe3ca392be0bbb8
Zhiyuan Ma <631253076@qq.com>
2023-01-07 02:15:32 -0500

    codeforces 1395C

---
+++ 1395/C. Boboniu and Bit Operations/Solution.java
@@ -0,0 +1,101 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) {
+        Kattio kattio = new Kattio();
+//        int t = kattio.nextInt();
+//        while (t > 0) {
+//            solve(kattio);
+//            t--;
+//        }
+        solve(kattio);
+        kattio.close();
+    }
+
+    public static void solve(Kattio kattio) {
+        int n = kattio.nextInt();
+        int m = kattio.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[m];
+        for (int i = 0; i < n; i++) {
+            a[i] = kattio.nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            b[i] = kattio.nextInt();
+        }
+        for (int res = 0; res < (1 << 9); res++) {
+            boolean can = true;
+            for (int i = 0; i < n; i++) {
+                boolean can2 = false;
+                for (int j = 0; j < m; j++) {
+                    int cur = a[i] & b[j];
+                    if ((res | cur) == res) {
+                        can2 = true;
+                        break;
+                    }
+                }
+                if (!can2) {
+                    can = false;
+                    break;
+                }
+            }
+            if (can) {
+                kattio.println(res);
+                return;
+            }
+        }
+    }
+
+    static class Kattio extends PrintWriter {
+        BufferedReader r;
+        StringTokenizer st;
+
+        // 标准 IO
+        Kattio() {
+            this(System.in, System.out);
+        }
+
+        Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+
+        // 文件 IO
+        Kattio(String input, String output) throws IOException {
+            super(output);
+            r = new BufferedReader(new FileReader(input));
+        }
+
+        // 在没有其他输入时返回 null
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    st = new StringTokenizer(r.readLine());
+                }
+                return st.nextToken();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
