f9fc1269c04689ea5b4436fafe613712eabbf1e6
Zhiyuan Ma <631253076@qq.com>
2023-01-19 21:37:31 -0500

    codeforces 1598D

---
+++ 1598/D. Training Session/Solution.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) {
+        Kattio kattio = new Kattio();
+        int t = kattio.nextInt();
+        while (t > 0) {
+            solve(kattio);
+            t--;
+        }
+//        solve(kattio);
+        kattio.close();
+    }
+
+    public static void solve(Kattio kattio) {
+        int n = kattio.nextInt();
+        int[] x = new int[n];
+        int[] y = new int[n];
+        int[] cx = new int[n + 1];
+        int[] cy = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            x[i] = kattio.nextInt();
+            y[i] = kattio.nextInt();
+            cx[x[i]]++;
+            cy[y[i]]++;
+        }
+        long res = (long) n * (n - 1) * (n - 2) / 6;
+        for (int i = 0; i < n; i++) {
+            res -= (long) (cx[x[i]] - 1) * (cy[y[i]] - 1);
+        }
+        kattio.println(res);
+    }
+
+    static class Kattio extends PrintWriter {
+        BufferedReader r;
+        StringTokenizer st;
+
+        // 标准 IO
+        Kattio() {
+            this(System.in, System.out);
+        }
+
+        Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+
+        // 文件 IO
+        Kattio(String input, String output) throws IOException {
+            super(output);
+            r = new BufferedReader(new FileReader(input));
+        }
+
+        // 在没有其他输入时返回 null
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    st = new StringTokenizer(r.readLine());
+                }
+                return st.nextToken();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
