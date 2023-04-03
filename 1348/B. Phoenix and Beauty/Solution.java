d19146f58bff99331367bef8bde137723d86aa73
Zhiyuan Ma <631253076@qq.com>
2023-04-03 01:10:11 -0400

    codeforces 1348B

---
+++ 1348/B. Phoenix and Beauty/Solution.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) {
+        Kattio kattio = new Kattio();
+        int t = kattio.nextInt();
+        while (t > 0) {
+            solve(kattio);
+            t--;
+        }
+//        solve(kattio);
+        kattio.close();
+    }
+
+    public static void solve(Kattio kattio) {
+        int n = kattio.nextInt();
+        int k = kattio.nextInt();
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(kattio.nextInt());
+        }
+        if (set.size() > k) {
+            kattio.println(-1);
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Integer integer : set) {
+            sb.append(integer).append(" ");
+        }
+        for (int i = 0; i < k - set.size(); i++) {
+            sb.append(1).append(" ");
+        }
+        kattio.println(n * k);
+        for (int i = 0; i < n; i++) {
+            kattio.print(sb);
+        }
+        kattio.println();
+    }
+
+    static class Kattio extends PrintWriter {
+        BufferedReader r;
+        StringTokenizer st;
+
+        // 标准 IO
+        Kattio() {
+            this(System.in, System.out);
+        }
+
+        Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+
+        // 文件 IO
+        Kattio(String input, String output) throws IOException {
+            super(output);
+            r = new BufferedReader(new FileReader(input));
+        }
+
+        // 在没有其他输入时返回 null
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    st = new StringTokenizer(r.readLine());
+                }
+                return st.nextToken();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
